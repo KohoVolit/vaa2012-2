d82546d97fbc054555b9cdbb2823e9f1327d6c86
Michal Skop <michal.skop@kohovolit.eu>
2013-04-22 16:23:11 +0200

    save all research questions

--- www/volba-prezidenta-cr-2013/save_research_post.php
+++ www/volba-prezidenta-cr-2013/save_research_post.php
@@ -2,7 +2,32 @@
 
 session_start();
 
-$json = json_encode($_REQUEST);
+
+if (!isset($_REQUEST))
+  $json = json_encode("nothing");
+else {
+  $data = array (
+    'input-q0' => '',
+    'input-q1' => '',
+    'input-q2' => '',
+    'input-q3' => '',
+    'input-q4' => '',
+    'input-q5' => '',
+    'input-q6' => '',
+    'input-q7' => '',
+    'input-q8' => '',
+    'input-q9' => '',
+    'input-q10' => '',
+    'input-q11' => '',
+    'input-q12' => '',
+    'input-q13' => '',
+  );
+  foreach ($_REQUEST as $key=>$row) {
+    $data[$key] = $row;
+  }
+  $json = json_encode($data);
+  
+}
 $handle = fopen("research_post.txt","a+");
 $str = session_id() . "\t" . "\t" . date("Y-m-d H:i:s") . "\t" . $json . "\n";
 
