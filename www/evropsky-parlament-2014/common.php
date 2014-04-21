96ded69bb48b8e903d0815ed15786a9ab9abab3d
Michal Skop <michal.skop@kohovolit.eu>
2014-04-21 23:57:46 +0200

    allowing dot

--- www/evropsky-parlament-2014/common.php
+++ www/evropsky-parlament-2014/common.php
@@ -10,7 +10,7 @@ error_reporting(0);
 * http://stackoverflow.com/questions/1587695/sanitize-get-parameters-to-avoid-xss-and-other-attacks
 */
 function sanitize($input) {
-    return preg_replace('/[^-a-zA-Z0-9_]/', '', $input);;
+    return preg_replace('/[^-a-zA-Z0-9_.]/', '', $input);;
 }
 
 /**
