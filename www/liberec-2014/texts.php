edc1a356a59b08195a3f2dec403577abdf9b4d46
Michal Skop <michal.skop@kohovolit.eu>
2014-09-30 01:54:01 +0200

    praga, plzen, liberec

---
+++ www/liberec-2014/texts.php
@@ -0,0 +1,107 @@
+<?php
+/**
+* Texts (for translation)
+*/
+$text = array(
+  
+  'election_code' => 'liberec-2014',
+  'template_code' => 'city-2014',
+  'lang' => 'cs',
+  //meta
+  'author' => 'KohoVolit.eu',
+  'description' => 'Volby 2014 se blíží! Spočítejte si koho volit!',
+  'keywords' => 'volby 2014, senátní volby, volební kalkulačka, volební test',
+  //fb
+  'og:image' => 'http://volebnikalkulacka.cz/image/logo.jpg',
+  //'og:title' => 'Volební kalkulačka',
+  //'og:url' => 'http://volebnikalkulacka.cz',
+  //'og:site_name' => 'Volební kalkulačka',
+  'author_link' => 'http://kohovolit.eu',
+  'licence' => 'Creative Commons BY 4.0',
+  'year' => '2014',
+  
+  'footer_text' => '<a href="http://kohovolit.eu">KohoVolit.eu</a> pro Vás vytváří volební kalkulačky již od roku 2006<!--<br/>Proudly part of <a href="http://www.votematch.eu/">VoteMatchEurope</a>-->',
+  
+
+  'calc' => 'Volební kalkulačka',
+  'info' => 'Info',
+  'url' => 'http://volebnikalkulacka.cz',  
+  'title' => 'Volební kalkulačka 2014 | volby v Liberci' ,
+
+   //PAGE
+  'calc_name' => 'Volební kalkulačka - volby 2014 Liberec',
+  'page_question_yes' => 'ANO',
+  'page_question_no' => 'NE',
+  'page_question_neutral' => 'nevím',
+  'page_question_neutral_add' => ' / je mi to jedno',
+  'page_important_questions' => 'Důležité otázky', 
+  'page_important_legend' => '<strong>Vyberte otázky, které jsou pro Vás nejdůležitější</strong>.  Zpřesníte tím výsledek. Tento krok není povinný a lze přeskočit.',
+  'page_important_skip' => 'Přeskočit',
+  'page_important_results' => 'Výsledky', 
+  
+  'ie8' => 'Omlouváme se, ale Volební kalkulačka nemusí fungovat v prohlížečích Internet Explorer 8 a starších',
+  'opera11' =>' Omlouváme se, ale Volební kalkulačka nemusí fungovat v prohlížečích Opera 11 a starších',
+
+   //MATCH
+   'match_save' => 'Uložit svoje hlasování',
+ 
+   'result_winner_share' => 'Sdílej výsledky na Facebooku',
+   'result_share' => 'Sdílejte výsledky',
+   
+   'comparison_parties_eu' => 'Porovnání se stranami v EU',
+   'comparison_eu_explained' => '15 otázek, které jsou v české volební kalkulačce, je shodných i ve volebních kalkulačkách v jiných zemích EU. Které strany jsou Vám tam nejblíže?',
+   'missing_table_description' => 'Následující kandidáti <strong>na otázky zatím neodpověděli</strong>:',
+   
+   //fb
+   'result_fb_appid' => "200785490055088",
+  'result_fb_picture_link' => 'http://volebnikalkulacka.cz/liberec-2014/image/logo/',
+  'result_fb_name' => 'Ve Volební kalkulačce Liberec mám nejvyšší shodu:',
+  'result_fb_description_1' => 'Dle Volební kalkulačky pro Liberec se se mnou nejvíce shodují: ',
+  'result_fb_description_match' => 'shoda',
+  'result_fb_description_and' => 'a',
+  'result_fb_description_only' => 'jen',
+  'result_fb_description_2' => 'Nejhorší je ',
+  'result_fb_description_3' => 'S kým se shodnete na VolebniKalkulacka.cz?',
+  'result_fb_redirect_link' => 'http://volebnikalkulacka.cz/',
+  'result_fb_link' => 'http://volebnikalkulacka.cz/',
+  'result_fb_action' => "[{'name':'Koho volit?','link':'http://volebnikalkulacka.cz/'}]",   
+   
+   //tabs
+   'result_results' => 'Výsledky',
+   'result_comparison' => 'Porovnání',
+   //'result_eu' => 'Výsledky v EU',
+   
+   //support (DMS)
+   'result_support' => '<strong>Podpořte tvorbu nezávislých
+  Volebních kalkulaček.</strong><br/>
+  Pošlete SMS na&nbsp;číslo&nbsp;<strong><span class="colorstrong">87777</span></strong>
+  s&nbsp;textem <strong><span class="colorstrong">DMS&nbsp;KOHOVOLIT</span></strong>
+  (cena 30Kč, z toho 28Kč pro
+   sdružení KohoVolit.eu) nebo 
+   <strong><span class="colorstrong">DMS&nbsp;ROK&nbsp;KOHOVOLIT</span></strong>
+   (automaticky každý měsíc po 1 rok)',
+   
+   //comparison
+   'result_close' => 'Zavřít',
+   'result_yes' => 'Ano',
+   'result_no' => 'Ne',
+   'result_question' => 'Otázka',
+   
+   //save
+   'result_save_description' => 'K výsledkům svého hlasování se můžete kdykoliv vrátit, pokud si zkopírujete a uložíte celou tuto adresu:',
+   
+   //google analytics tracking id
+   'google_tracking_id' => 'UA-8592359-7',    
+
+
+  'result_twitter_data_lang' => 'cs',
+  'result_twitter_text_1' => 'Volební kalkulačka LBC:1.',
+  //'result_twitter_text_2' => '1.',
+  //'result_twitter_and' => 'a',
+  'result_twitter_text_3' => ',nejhůř ',
+  'result_twitter_account' => 'KohoVolitEU',
+  'result_twitter_hashtags' => 'volby',
+  'result_twitter_share' => 'Tweetni',
+  
+  
+);
