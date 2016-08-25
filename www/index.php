4be9bb0d99ae282aa79607eac032b419ae0c4515
Michal Skop <michal.skop@kohovolit.eu>
2016-08-25 05:02:17 +0200

    fp to https

--- www/index.php
+++ www/index.php
@@ -10,9 +10,9 @@
     <meta name="author" content="KohoVolit.eu">
     <link type="image/x-icon" href="image/favicon.ico" rel="shortcut icon">
 
-    <meta property="og:image" content="http://volebnikalkulacka.cz/image/logo.jpg"/>
+    <meta property="og:image" content="https://volebnikalkulacka.cz/image/logo.jpg"/>
 	<meta property="og:title" content="Volební kalkulačka"/>
-	<meta property="og:url" content="http://volebnikalkulacka.cz"/>
+	<meta property="og:url" content="https://volebnikalkulacka.cz"/>
 	<meta property="og:site_name" content="Volební kalkulačka"/>
 	<meta property="og:type" content="website"/>
 
@@ -24,7 +24,7 @@
 
     <!-- Add custom CSS here -->
     <link href="css/stylish-portfolio.min.css" rel="stylesheet">
-	<link type="text/css" rel="stylesheet" href="http://fonts.googleapis.com/css?family=Open+Sans:400,700,800|Roboto+Slab:400,700&subset=latin,latin-ext">
+	<link type="text/css" rel="stylesheet" href="//fonts.googleapis.com/css?family=Open+Sans:400,700,800|Roboto+Slab:400,700&subset=latin,latin-ext">
 	<link rel="stylesheet" href="css/vaa2014.css">
 	<style>
 	body {
@@ -84,45 +84,28 @@
               <p class="lead">
                   Volební kalkulačky pro krajské volby 2016.
                   <p>
-                  <button href="<?php echo $randpath; ?>/5/" class="btn btn-lg btn-success" style="white-space: normal;" disabled="disabled" ><img src="http://www.europarl.europa.eu/ep_framework/img/flag/flag_icon_cz.gif"> BUDOU SPUŠTĚNY V TÝDNU 12.-19.9.2016</button>
+                  <button href="<?php echo $randpath; ?>/5/" class="btn btn-lg btn-success" style="white-space: normal;" disabled="disabled" ><img src="https://www.europarl.europa.eu/ep_framework/img/flag/flag_icon_cz.gif"> BUDOU SPUŠTĚNY V TÝDNU 12.-19.9.2016</button>
                 <p class="lead">
                     Volební kalkulačka pro senátní volby 2016.
                     <p>
-                    <button href="<?php echo $randpath; ?>/5/" class="btn btn-lg btn-success btn-disabled" style="white-space: normal;" disabled="disabled"><img src="http://www.europarl.europa.eu/ep_framework/img/flag/flag_icon_cz.gif"> BUDE SPUŠTĚNA V TÝDNU 12.-19.9.2016</button>
+                    <button href="<?php echo $randpath; ?>/5/" class="btn btn-lg btn-success btn-disabled" style="white-space: normal;" disabled="disabled"><img src="https://www.europarl.europa.eu/ep_framework/img/flag/flag_icon_cz.gif"> BUDE SPUŠTĚNA V TÝDNU 12.-19.9.2016</button>
 
             <p class="lead">
                 Inventura hlasování z Parlamentu Vám pomůže zjistit, kteří poslanci a senátoři Vás <em>doopravdy</em> zastupovali v roce 2015.
                 <!--                               Online volební test Vám zabere přibližně 5 minut.-->
 		    <p>
-		    <a href="<?php echo $randpath; ?>/inventura-hlasovani-2015/" class="btn btn-lg btn-success" style="white-space: normal;" ><img src="http://www.europarl.europa.eu/ep_framework/img/flag/flag_icon_cz.gif"> SPUSTIT INVENTURU HLASOVÁNÍ - PARLAMENT</a>
+		    <a href="<?php echo $randpath; ?>/inventura-hlasovani-2015/" class="btn btn-lg btn-success" style="white-space: normal;" ><img src="https://www.europarl.europa.eu/ep_framework/img/flag/flag_icon_cz.gif"> SPUSTIT INVENTURU HLASOVÁNÍ - PARLAMENT</a>
 
             <p class="lead">
                 Inventura hlasování z Evropského parlamentu Vám ukáže, kteří europoslanci Vás <em>doopravdy</em> zastupovali v letech 2014-2015.
                 <p>
-                <a href="<?php echo $randpath; ?>/inventura-hlasovani-evropsky-parlament-2014-2015/" class="btn btn-lg btn-success" style="white-space: normal;" ><img src="http://www.europarl.europa.eu/ep_framework/img/flag/flag_icon_eu.gif"> SPUSTIT INVENTURU HLASOVÁNÍ - EVROPSKÝ PARLAMENT</a>
+                <a href="<?php echo $randpath; ?>/inventura-hlasovani-evropsky-parlament-2014-2015/" class="btn btn-lg btn-success" style="white-space: normal;" ><img src="https://www.europarl.europa.eu/ep_framework/img/flag/flag_icon_eu.gif"> SPUSTIT INVENTURU HLASOVÁNÍ - EVROPSKÝ PARLAMENT</a>
 
 
 
 
 
 
-				   <p>
-
-				   <div class=""><iframe src="//www.facebook.com/plugins/like.php?href=https%3A%2F%2Fwww.facebook.com%2FKohoVolit.eu&amp;width=100&amp;layout=box_count&amp;action=like&amp;show_faces=false&amp;share=false&amp;height=65&amp;appId=469801333121609" scrolling="no" frameborder="0" style="border:none; overflow:hidden; width:100px; height:65px;" allowTransparency="true"></iframe><a href="https://twitter.com/share" class="twitter-share-button" data-url="http://volebnikalkulacka.cz" data-text="Zkuste taky Volební kalkulačku!" data-via="KohoVolitEU" data-hashtags="EP2014" data-count ="vertical">Tweet</a>
-<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script><!-- Place this tag where you want the +1 button to render. -->
-<div class="g-plusone" data-size="tall" data-href="http://volebnikalkulacka.cz/"></div>
-
-<!-- Place this tag after the last +1 button tag. -->
-<script type="text/javascript">
-  (function() {
-    var po = document.createElement('script'); po.type = 'text/javascript'; po.async = true;
-    po.src = 'https://apis.google.com/js/platform.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(po, s);
-  })();
-</script></div></div>
-        </div><p id="support" class="support text-center small"><strong>Podpořte tvorbu nezávislých<br>Volebních kalkulaček.</strong><br>Pošlete SMS na číslo <strong><font color="#F8941A">87777</font></strong><br> s textem <strong><font color="#F8941A">DMS KOHOVOLIT</font></strong> <br>(cena 30Kč, z toho 27Kč pro<br> sdružení KohoVolit.eu) nebo <br><strong><font color="#F8941A">DMS ROK KOHOVOLIT</font></strong> <br>(automaticky každý měsíc po 1 rok)</p>
-    </div>
-    <!-- /Full Page Image Header Area -->
 
 
 
