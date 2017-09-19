ac4d2a0bf6c83ab4914326746a02132e7b15edbc
Michal Skop <michal.skop@kohovolit.eu>
2017-09-20 00:32:29 +0200

    emails backup

---
+++ www/volby-2017-simple/match/emails.php
@@ -0,0 +1,14 @@
+<?php
+$arr = [
+    date("Y-m-d H:i:s"),
+    $_GET['vkid'],
+    $_GET['calc'],
+    $_GET['campaign'],
+    $_GET['value'],
+    $_GET['type'],
+    $_GET['attributes']
+];
+$file = fopen('emails.csv','a');
+fputcsv($file,$arr);
+fclose($file);
+ ?>
