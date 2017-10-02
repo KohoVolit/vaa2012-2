78b37f3275326f8009ea8ff0f269faf16751529e
Michal Skop <michal.skop@kohovolit.eu>
2017-10-02 02:02:34 +0200

    ab lower text sharing

---
+++ www/volby-2017-simple/match/action.php
@@ -0,0 +1,12 @@
+<?php
+$arr = [
+    date("Y-m-d H:i:s"),
+    $_GET['vkid'],
+    $_GET['calc'],
+    $_GET['action'],
+    $_GET,
+    $_SERVER['REMOTE_ADDR']
+];
+$file = fopen('actions.csv','a');
+fputcsv($file,$arr);
+fclose($file);
