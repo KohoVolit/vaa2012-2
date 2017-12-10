baf5f0614618c9e8a1eaeb5224fff0107a5ab995
Michal Skop <michal.skop@kohovolit.eu>
2017-12-10 22:07:05 +0100

    build

--- vue/prezidentske-volby-2018/config/index.js
+++ vue/prezidentske-volby-2018/config/index.js
@@ -55,7 +55,7 @@ module.exports = {
     // Paths
     assetsRoot: path.resolve(__dirname, '../dist'),
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '/cs/prezidentske-volby-2018/',
 
     /**
      * Source Maps
