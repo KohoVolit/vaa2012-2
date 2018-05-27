2ee92cdf5a1361757c79345f4a2ccbeef39d1276
Michal Skop <michal.skop@kohovolit.eu>
2018-05-27 11:49:15 +0200

    pics CORS 3

--- www/hackathon-2018/logo/index.php
+++ www/hackathon-2018/logo/index.php
@@ -4,7 +4,10 @@ header('Access-Control-Allow-Methods: GET');
 header("Access-Control-Allow-Headers: X-Requested-With");
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 26 Jul 1997 05:00:00 GMT');
+header('Content-type: image/png');
 
 $showimage = $_GET['p'];
+// if (not(strpos($showimage, '..') or strpos($showimage, '/')))
+    readfile($showimage);
+
 ?>
-<img src="<?php echo $showimage; ?>" />
