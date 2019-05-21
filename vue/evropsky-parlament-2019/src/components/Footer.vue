c9b5d0d4de9de4fe72590e3f75b2c95550eb9b35
Michal Skop <michal.skop@kohovolit.eu>
2019-05-21 12:12:31 +0200

    update

--- vue/evropsky-parlament-2019/src/components/Footer.vue
+++ vue/evropsky-parlament-2019/src/components/Footer.vue
@@ -8,6 +8,7 @@
                 <div class="col-md-6 text-center">
                     <a href="http://kohovolit.eu" title="KohoVolit.eu" id="footer-kohovolit-logo" target="_blank"><img src="https://volebnikalkulacka.cz/image/logo.png" alt="logo kohovolit.eu"></a>
 					<p class="lead black">Volební kalkulačky pro Vás už od roku 2006 vytváří sdružení <a href="http://kohovolit.eu" target="_blank">KohoVolit.eu</a></p>
+                    <p class="lead black"><a href="https://cesko.digital" target="_blank" alt="Česko.Digital"><img src="@/assets/img/cesko-digital_logox58.png" /></a><br />Projekt v rámci <a href="https://cesko.digital" target="_blank" alt="Česko.Digital">Česko.Digital</a></p>
                     <hr>
 					<p class="black">Creative Commons BY 4.0 | <a href="http://kohovolit.eu" id="footer-kohovolit-licence">KohoVolit.eu</a> 2019</p>
                 </div>
