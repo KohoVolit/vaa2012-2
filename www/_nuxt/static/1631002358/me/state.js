4f9d2ece6372ec64046d450aba76e11ab725d000
Michal Skop <michal.skop@kohovolit.eu>
2021-09-07 10:14:21 +0200

    frontpage fix

---
+++ www/_nuxt/static/1631002358/me/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631002358",layout:"default",error:a,serverRendered:true,routePath:"\u002Fme",config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"c9ae515d"}}}}(null));
