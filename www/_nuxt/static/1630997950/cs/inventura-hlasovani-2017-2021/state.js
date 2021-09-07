4726f2780aab2df0b0f7410ac83d36a97f644221
Michal Skop <michal.skop@kohovolit.eu>
2021-09-07 09:00:44 +0200

    launch inventory

---
+++ www/_nuxt/static/1630997950/cs/inventura-hlasovani-2017-2021/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1630997950",layout:"default",error:{statusCode:404,path:a,message:"This page could not be found"},serverRendered:false,routePath:a,config:{_app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:null},content:{dbHash:"c7f060bf"}}}}("\u002Fcs\u002Finventura-hlasovani-2017-2021"));
