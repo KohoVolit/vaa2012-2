bba0aa1eec72cba7c855b11001aacc8a7faecc58
Michal Skop <michal.skop@kohovolit.eu>
2021-09-09 22:15:50 +0200

    ulti

---
+++ www/_nuxt/static/1631217554/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a,b){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1631217554",layout:"default",error:a,serverRendered:true,routePath:b,config:{_app:{basePath:b,assetsPath:"\u002F_nuxt\u002F",cdnURL:a},content:{dbHash:"160ca42c"}}}}(null,"\u002F"));
