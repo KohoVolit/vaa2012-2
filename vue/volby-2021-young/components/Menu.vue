602b9dc698525aad87777e62f7e11fb5af4baa14
Michal Skop <michal.skop@kohovolit.eu>
2021-09-13 16:05:37 +0200

    update young header

--- vue/volby-2021-young/components/Menu.vue
+++ vue/volby-2021-young/components/Menu.vue
@@ -15,7 +15,7 @@
             <a class="nav-link active" aria-current="page" href="https://www.volebnikalkulacka.cz/">🏡 Volební kalkulačky 2021</a>
           </li>
           <li class="nav-item">
-            <NuxtLink class="nav-link active" aria-current="page" to="/">Volební kalkulačka 2021</NuxtLink>
+            <NuxtLink class="nav-link active" aria-current="page" to="/">Mladí volí, volební kalkulačka 2021</NuxtLink>
           </li>
           <li class="nav-item">
             <NuxtLink class="nav-link" to="/me/"><span class="text-success">Moje kalkulačky 🔥</span></NuxtLink>
