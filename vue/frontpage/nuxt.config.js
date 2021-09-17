c8fa4710c099ccf1dabaf3b8328c597dafeb75df
Michal Skop <michal.skop@kohovolit.eu>
2021-09-17 23:57:59 +0200

    frontpage SEO

--- vue/frontpage/nuxt.config.js
+++ vue/frontpage/nuxt.config.js
@@ -14,12 +14,12 @@ export default {
       { hid: 'format-detection', name: 'format-detection', content: 'telephone=no' },
       { hid: 'author', name: 'author', content: 'Michal Škop, KohoVolit.eu' },
       { hid: "og:description", property:"og:description", content:"Volební kalkulačka 2021. Nejužitečnějších 10 minut před volbami."},
-      { hid: "og:title", nproperty: "og:title", content:"Volební kalkulačka 2021"},
+      { hid: "og:title", property: "og:title", content:"Volební kalkulačka 2021"},
       { hid: "og:site_name", property:"og:site_name", content:"Volební kalkulačka 2021"},
       { hid: "fb:app_id", property:"fb:app_id", content:"200785490055088" },
       { hid: "og:image", property:'og:image', content: "https://volebnikalkulacka.azureedge.net/senat-2016/image/bg.jpg" },
       { hid: "og:type", property:"og:type", content:"website" },
-      { hid: "og:type", property:"og:url", content:"https://www.volebnikalkulacka.cz/" },
+      { hid: "og:url", property:"og:url", content:"https://www.volebnikalkulacka.cz/" },
       { hid: "apple-mobile-web-app-title", name: "apple-mobile-web-app-title", property:"apple-mobile-web-app-title", content:"Volební kalkulačka" }
       
     ],
---
+++ www/_nuxt/static/1631915811/manifest.js
@@ -0,0 +1,1 @@
+__NUXT_JSONP__("manifest.js", {routes:["\u002F","\u002Fme","\u002Farchive","\u002Fheroes","\u002Fabout"]})
